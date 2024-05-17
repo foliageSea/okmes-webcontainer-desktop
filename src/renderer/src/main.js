9e5bc47207cffd634bbc612d25601eb6b000fb75
foliageSea <1742262457@qq.com>
2024-05-17 08:35:59 +0800

    feat:默认暗黑模式

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -4,6 +4,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 import { createPinia } from 'pinia'
